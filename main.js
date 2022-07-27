969de0ba75142e76d19951e8c806ba08b10b8539
felipesimon <95724153+felipesimon@users.noreply.github.com>
2022-07-26 21:20:30 -0300

    avanços no js
    adição da lógica para marcação dos itens do menu superior indicando a seção da página.

--- main.js
+++ main.js
@@ -5,7 +5,38 @@ function onScroll() {
     showNavOnScroll()
     showBackToTopButtonOnScroll()
 
+    activateMenuAtCurrentSection(home)
+    activateMenuAtCurrentSection(services)
+    activateMenuAtCurrentSection(about)
 }
+
+
+function activateMenuAtCurrentSection(section) {
+    const targetLine = scrollY + innerHeight / 2
+
+    //verificando se o início da seção passou da targetLine
+    const sectionStart = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    
+    const sectionStartReachOrPassedTargetline = targetLine >= sectionStart
+
+    //verificando se o fim da seção está abaixo da targetLine
+    const sectionEndsAt = sectionStart + sectionHeight
+
+    const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+    //limites da seção
+    const sectionBoundaries = sectionStartReachOrPassedTargetline && !sectionEndPassedTargetline
+
+    const sectionId = section.getAttribute('id')
+    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+    menuElement.classList.remove('active')
+    if (sectionBoundaries) {
+        menuElement.classList.add('active')
+    }
+}
+
 
 function showNavOnScroll() {
     if (scrollY > 0) {
